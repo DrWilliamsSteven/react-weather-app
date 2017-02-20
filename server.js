1599aafa35534b679e6b185ce5c983ccf9c94afe
Steven Williams <dr.williams.steven@gmail.com>
2017-02-20 23:17:48 +0000

    Update to force use http

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+app.use(function(req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+})
+
 app.use(express.static('public'));
 
 app.listen(PORT, function() {
